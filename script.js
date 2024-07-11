c5d45672026e19044c9afa76b8119ac848f88851
ViperNetwork <62898659+ViperNetwork@users.noreply.github.com>
2024-07-11 22:26:09 +0300

    2

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-document.getElementById('apply-btn').addEventListener('click', function() {
-    window.location.href = 'https://discord.gg/f2uGWaN3uY';
-});
-
 import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js';
 
     neonCursor({
@@ -17,3 +13,6 @@ import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-t
         sleepTimeCoefX: 0.0025,
         sleepTimeCoefY: 0.0025
     });
+document.getElementById('apply-btn').addEventListener('click', function() {
+    window.location.href = 'https://discord.gg/f2uGWaN3uY';
+});
